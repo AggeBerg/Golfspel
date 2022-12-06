1831089f3b3e407805fc835ed750549f2b83750b
Agathon Bergroth <Agathon.Bergroth@abbgymnasiet.se>
2022-12-06 15:40:06 +0100

    Fixed Esc Menu Position

--- Assets/Scripts/Player/PauseSinglePlayer.cs
+++ Assets/Scripts/Player/PauseSinglePlayer.cs
@@ -17,6 +17,9 @@ public class PauseSinglePlayer : MonoBehaviour
     public void OpenMenu(){
         clone = (GameObject )Instantiate(escapeMenu, new Vector3(0, 0, 0), Quaternion.identity);
         clone.transform.SetParent(UIParent.transform);
+        RectTransform rt = clone.GetComponent<RectTransform>();
+        rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 1920, rt.rect.width);
+        rt.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 1080, rt.rect.height);
         Time.timeScale=0;
         menuIsOpen = true;
     }
